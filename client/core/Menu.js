eace19c101e9905b312ff8421614db8da1447535
amudaryo <daria.muracheva@gmail.com>
2021-07-02 00:57:30 +0200

    add test feature FE and BE

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -1,25 +1,25 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import HomeIcon from "@material-ui/icons/Home";
-import Button from "@material-ui/core/Button";
-import auth from "./../auth/auth-helper";
-import { Link, withRouter } from "react-router-dom";
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
+import Button from '@material-ui/core/Button';
+import auth from './../auth/auth-helper';
+import { Link, withRouter } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   cursive: {
-    fontFamily: "Yellowtail",
+    fontFamily: 'Yellowtail',
     paddingLeft: theme.spacing(1),
     paddingRight: theme.spacing(1),
   },
 }));
 
 const isActive = (history, path) => {
-  if (history.location.pathname == path) return { color: "#e0c0e0" };
-  else return { color: "#ffffff" };
+  if (history.location.pathname == path) return { color: '#e0c0e0' };
+  else return { color: '#ffffff' };
 };
 
 const Menu = withRouter(({ history }) => {
@@ -31,24 +31,24 @@ const Menu = withRouter(({ history }) => {
           Illume
         </Typography>
         <Link to="/">
-          <IconButton aria-label="Home" style={isActive(history, "/")}>
+          <IconButton aria-label="Home" style={isActive(history, '/')}>
             <HomeIcon />
           </IconButton>
         </Link>
         <Link to="/users">
-          <Button size="small" style={isActive(history, "/users")}>
+          <Button size="small" style={isActive(history, '/users')}>
             Users
           </Button>
         </Link>
         {!auth.isAuthenticated() && (
           <span>
             <Link to="/signup">
-              <Button size="small" style={isActive(history, "/signup")}>
+              <Button size="small" style={isActive(history, '/signup')}>
                 Sign up
               </Button>
             </Link>
             <Link to="/signin">
-              <Button size="small" style={isActive(history, "/signin")}>
+              <Button size="small" style={isActive(history, '/signin')}>
                 Sign In
               </Button>
             </Link>
@@ -56,20 +56,22 @@ const Menu = withRouter(({ history }) => {
         )}
         {auth.isAuthenticated() && (
           <span>
-            <Link to={"/user/" + auth.isAuthenticated().user._id}>
+            <Link to={'/user/' + auth.isAuthenticated().user._id}>
               <Button
+                size="small"
                 style={isActive(
                   history,
-                  "/user/" + auth.isAuthenticated().user._id
+                  '/user/' + auth.isAuthenticated().user._id
                 )}
               >
-                My Profile
+                Profile
               </Button>
             </Link>
             <Button
+              size="small"
               color="inherit"
               onClick={() => {
-                auth.clearJWT(() => history.push("/"));
+                auth.clearJWT(() => history.push('/'));
               }}
             >
               Sign out
